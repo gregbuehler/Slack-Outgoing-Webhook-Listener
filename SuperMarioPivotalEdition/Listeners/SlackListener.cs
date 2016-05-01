4dab6165997d99495ef67c5909ddd9f59cd4435b
DumpsterDoofus <dumpsterdoofus@gmail.com>
2016-04-30 21:51:38 -0700

    Now you don't need semicolons after commads. If you use them anyways, they get trimmed out, and it still works.

--- SuperMarioPivotalEdition/Listeners/SlackListener.cs
+++ SuperMarioPivotalEdition/Listeners/SlackListener.cs
@@ -72,8 +72,9 @@ namespace SuperMarioPivotalEdition
         private string ProcessSlackCommand(NameValueCollection form)
         {
             var triggerWord = form["trigger_word"].ToLower();
-            var splitFormText = form["text"].Split(new[] {':'}, 2);
-            var formTextContent = splitFormText.Length == 2 ? splitFormText[1] : "";
+            var splitFormText = form["text"].Split(new[] {triggerWord}, StringSplitOptions.RemoveEmptyEntries);
+            var formTextContent = splitFormText.FirstOrDefault()?.Trim(' ', '#', ':', '<', '>');
+            Console.WriteLine($"formTextContent:{formTextContent}");
             var channel = form["channel_name"];
             var channelInfo = _databaseClient.GetChannelInfoFromChannelName(channel);
             var token = form["token"];
@@ -86,19 +87,19 @@ namespace SuperMarioPivotalEdition
 **Pivotal commands**:
 *help* - Displays command help.
 *info* - Displays this channel's associated Pivotal info.
-*set project id:123* - Sets this channel's associated Pivotal Project ID to 123.
-*add tasks:12345* - Adds default tasks to story ID 12345.
-*add story:Giant Beetle* - Creates a new Pivotal issue with name ""Giant Beetle"" with default tasks.
-*add default task:Check exhaust ports* - Adds a new task to your team's default tasks.
+*set project id 123* - Sets this channel's associated Pivotal Project ID to 123.
+*add tasks 12345* - Adds default tasks to story ID 12345.
+*add story Giant Beetle* - Creates a new Pivotal issue with name ""Giant Beetle"" with default tasks.
+*add default task Check exhaust ports* - Adds a new task to your team's default tasks.
 *clear default tasks* - Clears default task list.
-*set default tasks from json:*`[""task1"", ""task2""]` - Parses a JSON array and sets it as the default tasks. Useful for setting tasks all at once.
+*set default tasks from json *`[""task1"", ""task2""]` - Parses a JSON array and sets it as the default tasks. Useful for setting tasks all at once.
 
 **Random commands**
 *random fractal* - Posts a random root-finder fractal.
-*cat bomb:2* - Posts 2 cat pictures. Currently Slack only unfurls at most 3 images per post.
-*youtube:cats and dogs* - Searches YouTube for ""cats and dogs"" and returns a random video from the top 10 results.
-*imgur:catnip* - Searches Imgur for ""catnip"" and returns a random image from the top 50-ish results.
-*google vision:[URL of some image]* - Displays a barchart of Google Cloud Vision's interpretation of the most likely features it thinks are in the image.";
+*add cats 2* - Posts 2 cat pictures. Currently Slack only unfurls at most 3 images per post.
+*youtube cats and dogs* - Searches YouTube for ""cats and dogs"" and returns a random video from the top 10 results.
+*imgur catnip* - Searches Imgur for ""catnip"" and returns a random image from the top 50-ish results.
+*google vision [URL of some image]* - Displays a barchart of Google Cloud Vision's interpretation of the most likely features it thinks are in the image.";
                     break;
                 case "info":
                     response = $"```{channelInfo}```";
@@ -107,7 +108,7 @@ namespace SuperMarioPivotalEdition
                     response = _pivotalClient.PostStory(channelInfo, formTextContent).ShortResponseMessage;
                     break;
                 case "add tasks":
-                    var allSucceeded = _pivotalClient.PostDefaultTasks(channelInfo, formTextContent.Trim(' ', '#')).Aggregate(true, (b, resp) => b && resp.IsSuccessful);
+                    var allSucceeded = _pivotalClient.PostDefaultTasks(channelInfo, formTextContent).Aggregate(true, (b, resp) => b && resp.IsSuccessful);
                     response = allSucceeded ? "Default tasks added." : "Error adding tasks.";
                     break;
                 case "add default task":
@@ -135,8 +136,8 @@ namespace SuperMarioPivotalEdition
                 case "random fractal":
                     response = _bitlyClient.ShortenUrl(_fractalClient.RandomFractal());
                     break;
-                case "cat bomb":
-                    var numCats = int.Parse(formTextContent.Trim());
+                case "add cats":
+                    var numCats = int.Parse(formTextContent);
                     var catRes = _catApiClient.GetCats(numCats);
                     response = catRes.data.images.Aggregate("", (s, image) => s + _bitlyClient.ShortenUrl(image.url) + "\n").Trim();
                     break;
@@ -147,7 +148,7 @@ namespace SuperMarioPivotalEdition
                     response = _bitlyClient.ShortenUrl(_imgurClient.SearchForRandom(formTextContent));
                     break;
                 case "google vision":
-                    var barchart = _googleVisionClient.AnnotateAndReturnUrlOfBarchart(formTextContent.Trim().Trim('<', '>')); // Slack puts brackets around URLs which are sent back in outgoing webhooks, so need to trim those out.
+                    var barchart = _googleVisionClient.AnnotateAndReturnUrlOfBarchart(formTextContent); // Slack puts brackets around URLs which are sent back in outgoing webhooks, so need to trim those out.
                     response = _bitlyClient.ShortenUrl(barchart);
                     break;
             }
