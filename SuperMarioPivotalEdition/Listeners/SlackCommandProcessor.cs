0a514227822ec9df0f1c702798ea1f4cc1787f08
DumpsterDoofus <dumpsterdoofus@gmail.com>
2016-11-04 14:33:05 -0700

    Split out the HTTP listening logic from the Slack command processing logic.

---
+++ SuperMarioPivotalEdition/Listeners/SlackCommandProcessor.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Linq;
+using ApiIntegrations.Clients;
+using ApiIntegrations.Models.Pivotal;
+using Newtonsoft.Json.Linq;
+using SuperMarioPivotalEdition.Data;
+using SuperMarioPivotalEdition.Models;
+
+namespace SuperMarioPivotalEdition.Listeners
+{
+    class SlackCommandProcessor
+    {
+        private readonly string _slackOutgoingWebhookToken;
+        private readonly BitlyClient _bitlyClient;
+        private readonly CatApiClient _catApiClient;
+        private readonly IDatabaseClient _databaseClient;
+        private readonly FractalClient _fractalClient;
+        private readonly GitHubClient _gitHubClient;
+        private readonly GoogleBooksClient _googleBooksClient;
+        private readonly GoogleVisionClient _googleVisionClient;
+        private readonly ImgurClient _imgurClient;
+        private readonly PivotalClient _pivotalClient;
+        private readonly TextBeltClient _textBeltClient;
+        private readonly YouTubeClient _youTubeClient;
+        private readonly Dictionary<string, Func<string>> _dict;
+        private string _formTextContent;
+        private SlackChannelInfo _channelInfo;
+
+        public SlackCommandProcessor()
+        {
+            _slackOutgoingWebhookToken = ConfigurationManager.AppSettings["SlackOutgoingWebhookToken"];
+            _dict = new Dictionary<string, Func<string>>
+            {
+                {"help", Help},
+                {"info", Info},
+                {"set project id", SetProjectId},
+                {"add tasks", AddTasks},
+                {"add story", AddStory},
+                {"add default task", AddDefaultTask},
+                {"clear default tasks", ClearDefaultTasks},
+                {"set default tasks from json", SetDefaultTasksFromJson},
+                {"random fractal", RandomFractal},
+                {"add cats", AddCats},
+                {"youtube" ,YouTube},
+                {"imgur", Imgur},
+                {"google books", GoogleBooks},
+                {"google vision", GoogleVision},
+                {"send text", SendText},
+                {"search repos", SearchRepos}
+            };
+            _databaseClient = !string.IsNullOrEmpty(ConfigurationManager.AppSettings["SqlConnectionString"])
+                ? (IDatabaseClient)new SqlDatabaseClient()
+                : new RavenDatabaseClient();
+            _pivotalClient = new PivotalClient();
+            _fractalClient = new FractalClient();
+            _bitlyClient = new BitlyClient();
+            _catApiClient = new CatApiClient();
+            _imgurClient = new ImgurClient();
+            _youTubeClient = new YouTubeClient();
+            _googleVisionClient = new GoogleVisionClient();
+            _googleBooksClient = new GoogleBooksClient();
+            _textBeltClient = new TextBeltClient();
+            _gitHubClient = new GitHubClient();
+
+        }
+
+        public string Process(NameValueCollection form)
+        {
+            var authorized = form["token"] == _slackOutgoingWebhookToken;
+            var triggerWord = form["trigger_word"].ToLower();
+            _formTextContent = form["text"].Substring(triggerWord.Length).Trim(' ', '#', ':', '<', '>');
+            _channelInfo = _databaseClient.GetSlackChannelInfo(form["channel_name"]);
+            return authorized ? _dict[triggerWord]() : "Unauthorized.";
+        }
+
+        private string SearchRepos()
+        {
+            return $"<{_gitHubClient.GetUrlToCodeSearchOrganizationRepos(_formTextContent)}|Search results.>";
+        }
+
+        private string SendText()
+        {
+            var temp = _formTextContent?.Split(new[] { ' ' }, 2);
+            var phoneNumber = temp[0].Trim();
+            var messageText = temp[1].Trim();
+            return _textBeltClient.SendMessage(phoneNumber, messageText);
+        }
+
+        private string GoogleVision()
+        {
+            var barchart = _googleVisionClient.AnnotateAndReturnUrlOfBarchart(_formTextContent);
+            return _bitlyClient.ShortenUrl(barchart);
+        }
+
+        private string GoogleBooks()
+        {
+            return _googleBooksClient.SearchForAndReturnRandomTextSnippet(_formTextContent);
+        }
+
+        private string Imgur()
+        {
+            return _bitlyClient.ShortenUrl(_imgurClient.SearchForRandom(_formTextContent));
+        }
+
+        private string YouTube()
+        {
+            return _youTubeClient.SearchForRandom(_formTextContent);
+        }
+
+        private string AddCats()
+        {
+            var numCats = int.Parse(_formTextContent);
+            var catRes = _catApiClient.GetCats(numCats);
+            return
+                catRes.data.images.Aggregate("", (s, image) => s + _bitlyClient.ShortenUrl(image.url) + "\n")
+                    .Trim();
+        }
+
+        private string RandomFractal()
+        {
+            return _bitlyClient.ShortenUrl(_fractalClient.RandomFractal());
+        }
+
+        private string SetDefaultTasksFromJson()
+        {
+            var jarray = JArray.Parse(_formTextContent);
+            var taskList = jarray.ToObject<List<string>>();
+            _channelInfo.DefaultTaskDescriptions = taskList;
+            _databaseClient.UpdateSlackChannelInfo(_channelInfo);
+            return $"Default tasks set to:```{jarray}```";
+        }
+
+        private string SetProjectId()
+        {
+            _channelInfo.PivotalProjectId = int.Parse(_formTextContent);
+            _databaseClient.UpdateSlackChannelInfo(_channelInfo);
+            return $"Pivotal project ID set to {_formTextContent}.";
+        }
+
+        private string ClearDefaultTasks()
+        {
+            _channelInfo.DefaultTaskDescriptions = new List<string>();
+            _databaseClient.UpdateSlackChannelInfo(_channelInfo);
+            return "Default task list cleared.";
+        }
+
+        private string AddDefaultTask()
+        {
+            _channelInfo.DefaultTaskDescriptions.Add(_formTextContent);
+            _databaseClient.UpdateSlackChannelInfo(_channelInfo);
+            return "New default task added.";
+        }
+
+        private string AddTasks()
+        {
+            var story = _pivotalClient.GetStoryWithProjectIdSafetyCheck(new Story
+            {
+                id = int.Parse(_formTextContent),
+                project_id = _channelInfo.PivotalProjectId
+            });
+            var tasks = _channelInfo.DefaultTaskDescriptions.Select(d => new ApiIntegrations.Models.Pivotal.Task { description = d }).ToArray();
+            var count = 0;
+            foreach (var task in tasks)
+            {
+                try
+                {
+                    _pivotalClient.PostTask(story, task);
+                    count++;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+            return count == tasks.Length
+                ? $"<{story.url}|Default tasks added.>"
+                : $"Error addings tasks. <{story.url}|{count} of {tasks.Length} tasks added.>";
+        }
+
+        private string AddStory()
+        {
+            var url = _pivotalClient.PostStory(new Story
+            {
+                name = _formTextContent,
+                project_id = _channelInfo.PivotalProjectId,
+                tasks = _channelInfo.DefaultTaskDescriptions.Select(d => new ApiIntegrations.Models.Pivotal.Task { description = d }).ToArray()
+            }).url;
+            return $"<{url}|New story created.>";
+        }
+
+        private string Info()
+        {
+            return $"```{_channelInfo}```";
+        }
+
+        private string Help()
+        {
+            return @"_All commands are case-insensitive_:
+**Pivotal commands**:
+*help* - Displays command help.
+*info* - Displays this channel's associated Pivotal info.
+*set project id 123* - Sets this channel's associated Pivotal Project ID to 123.
+*add tasks 12345* - Adds default tasks to story ID 12345.
+*add story Giant Beetle* - Creates a new Pivotal issue with name ""Giant Beetle"" with default tasks.
+*add default task Check exhaust ports* - Adds a new task to your team's default tasks.
+*clear default tasks* - Clears default task list.
+*set default tasks from json [""task1"", ""task2""]* - Parses a JSON array and sets it as the default tasks. Useful for setting tasks all at once.
+
+**Random commands**
+*random fractal* - Posts a random root-finder fractal.
+*add cats 2* - Posts 2 cat pictures. Currently Slack only unfurls at most 3 images per post.
+*youtube cats and dogs* - Searches YouTube for ""cats and dogs"" and returns a random video from the top 10 results.
+*imgur catnip* - Searches Imgur for ""catnip"" and returns a random image from the top 50-ish results.
+*google books blastoise*: Searches Google Books for a random book excerpt containing ""Blastoise"".
+*google vision [URL of some image]* - Displays a barchart of Google Cloud Vision's interpretation of the most likely features it thinks are in the image.
+*send text 5033071525 I'd like a cheeseburger* - Sends a text message to the phone number.
+*search repos blah* - Returns a GitHub URL that searches all organization repos code for ""blah"". GitHub removed the easy way to do this in 2013 for performance reasons.";
+        }
+
+
+    }
+}
--- SuperMarioPivotalEdition/Listeners/SlackListener.cs
+++ SuperMarioPivotalEdition/Listeners/SlackListener.cs
@@ -1,50 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Configuration;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Web;
-using ApiIntegrations.Clients;
-using ApiIntegrations.Models.Pivotal;
 using Newtonsoft.Json.Linq;
-using SuperMarioPivotalEdition.Data;
 
 namespace SuperMarioPivotalEdition.Listeners
 {
+    
+
     internal class SlackListener
     {
-        private readonly BitlyClient _bitlyClient;
-        private readonly CatApiClient _catApiClient;
-        private readonly IDatabaseClient _databaseClient;
-        private readonly FractalClient _fractalClient;
-        private readonly GitHubClient _gitHubClient;
-        private readonly GoogleBooksClient _googleBooksClient;
-        private readonly GoogleVisionClient _googleVisionClient;
+
         private readonly HttpListener _httpListener;
-        private readonly ImgurClient _imgurClient;
-        private readonly PivotalClient _pivotalClient;
-        private readonly string _slackOutgoingWebhookToken;
-        private readonly TextBeltClient _textBeltClient;
-        private readonly YouTubeClient _youTubeClient;
+        private readonly SlackCommandProcessor _slackCommandProcessor;
 
         public SlackListener()
         {
-            _databaseClient = !string.IsNullOrEmpty(ConfigurationManager.AppSettings["SqlConnectionString"])
-                ? (IDatabaseClient) new SqlDatabaseClient()
-                : new RavenDatabaseClient();
-            _pivotalClient = new PivotalClient();
-            _fractalClient = new FractalClient();
-            _bitlyClient = new BitlyClient();
-            _catApiClient = new CatApiClient();
-            _imgurClient = new ImgurClient();
-            _youTubeClient = new YouTubeClient();
-            _googleVisionClient = new GoogleVisionClient();
-            _googleBooksClient = new GoogleBooksClient();
-            _textBeltClient = new TextBeltClient();
-            _gitHubClient = new GitHubClient();
-            _slackOutgoingWebhookToken = ConfigurationManager.AppSettings["SlackOutgoingWebhookToken"];
+            _slackCommandProcessor = new SlackCommandProcessor();
             _httpListener = new HttpListener();
             _httpListener.Prefixes.Add(ConfigurationManager.AppSettings["ServerAddress"]);
             _httpListener.Start();
@@ -65,7 +39,7 @@ namespace SuperMarioPivotalEdition.Listeners
                 string responseBody;
                 try
                 {
-                    responseBody = ProcessSlackCommand(form);
+                    responseBody = _slackCommandProcessor.Process(form);
                 }
                 catch (Exception ex)
                 {
@@ -78,132 +52,6 @@ namespace SuperMarioPivotalEdition.Listeners
                     writer.Write(json);
                 }
             }
-        }
-
-        private string ProcessSlackCommand(NameValueCollection form)
-        {
-            var triggerWord = form["trigger_word"].ToLower();
-            var formTextContent = form["text"].Substring(triggerWord.Length).Trim(' ', '#', ':', '<', '>');
-            Console.WriteLine($"formTextContent:{formTextContent}");
-            var channel = form["channel_name"];
-            var channelInfo = _databaseClient.GetSlackChannelInfo(channel);
-            var token = form["token"];
-            var response = "";
-            if (token != _slackOutgoingWebhookToken) return response;
-            switch (triggerWord)
-            {
-                case "help":
-                    response = @"_All commands are case-insensitive_:
-**Pivotal commands**:
-*help* - Displays command help.
-*info* - Displays this channel's associated Pivotal info.
-*set project id 123* - Sets this channel's associated Pivotal Project ID to 123.
-*add tasks 12345* - Adds default tasks to story ID 12345.
-*add story Giant Beetle* - Creates a new Pivotal issue with name ""Giant Beetle"" with default tasks.
-*add default task Check exhaust ports* - Adds a new task to your team's default tasks.
-*clear default tasks* - Clears default task list.
-*set default tasks from json [""task1"", ""task2""]* - Parses a JSON array and sets it as the default tasks. Useful for setting tasks all at once.
-
-**Random commands**
-*random fractal* - Posts a random root-finder fractal.
-*add cats 2* - Posts 2 cat pictures. Currently Slack only unfurls at most 3 images per post.
-*youtube cats and dogs* - Searches YouTube for ""cats and dogs"" and returns a random video from the top 10 results.
-*imgur catnip* - Searches Imgur for ""catnip"" and returns a random image from the top 50-ish results.
-*google books blastoise*: Searches Google Books for a random book excerpt containing ""Blastoise"".
-*google vision [URL of some image]* - Displays a barchart of Google Cloud Vision's interpretation of the most likely features it thinks are in the image.
-*send text 5033071525 I'd like a cheeseburger* - Sends a text message to the phone number.
-*search repos blah* - Returns a GitHub URL that searches all organization repos code for ""blah"". GitHub removed the easy way to do this in 2013 for performance reasons.";
-                    break;
-                case "info":
-                    response = $"```{channelInfo}```";
-                    break;
-                case "add story":
-                    var url = _pivotalClient.PostStory(new Story
-                    {
-                        name = formTextContent,
-                        project_id = channelInfo.PivotalProjectId,
-                        tasks = channelInfo.DefaultTaskDescriptions.Select(d => new Task {description = d}).ToArray()
-                    }).url;
-                    response = $"<{url}|New story created.>";
-                    break;
-                case "add tasks":
-                    var story = _pivotalClient.GetStoryWithProjectIdSafetyCheck(new Story
-                    {
-                        id = int.Parse(formTextContent),
-                        project_id = channelInfo.PivotalProjectId
-                    });
-                    var tasks = channelInfo.DefaultTaskDescriptions.Select(d => new Task {description = d}).ToArray();
-                    var count = 0;
-                    foreach (var task in tasks)
-                    {
-                        try
-                        {
-                            _pivotalClient.PostTask(story, task);
-                            count++;
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine(ex.ToString());
-                        }
-                    }
-                    response = count == tasks.Length ? $"<{story.url}|Default tasks added.>" : $"Error addings tasks. <{story.url}|{count} of {tasks.Length} tasks added.>";
-                    break;
-                case "add default task":
-                    channelInfo.DefaultTaskDescriptions.Add(formTextContent);
-                    _databaseClient.UpdateSlackChannelInfo(channelInfo);
-                    response = "New default task added.";
-                    break;
-                case "clear default tasks":
-                    channelInfo.DefaultTaskDescriptions = new List<string>();
-                    _databaseClient.UpdateSlackChannelInfo(channelInfo);
-                    response = "Default task list cleared.";
-                    break;
-                case "set project id":
-                    channelInfo.PivotalProjectId = int.Parse(formTextContent);
-                    _databaseClient.UpdateSlackChannelInfo(channelInfo);
-                    response = $"Pivotal project ID set to {formTextContent}.";
-                    break;
-                case "set default tasks from json":
-                    var jarray = JArray.Parse(formTextContent);
-                    var taskList = jarray.ToObject<List<string>>();
-                    channelInfo.DefaultTaskDescriptions = taskList;
-                    _databaseClient.UpdateSlackChannelInfo(channelInfo);
-                    response = $"Default tasks set to:```{jarray}```";
-                    break;
-                case "random fractal":
-                    response = _bitlyClient.ShortenUrl(_fractalClient.RandomFractal());
-                    break;
-                case "add cats":
-                    var numCats = int.Parse(formTextContent);
-                    var catRes = _catApiClient.GetCats(numCats);
-                    response =
-                        catRes.data.images.Aggregate("", (s, image) => s + _bitlyClient.ShortenUrl(image.url) + "\n")
-                            .Trim();
-                    break;
-                case "youtube":
-                    response = _youTubeClient.SearchForRandom(formTextContent);
-                    break;
-                case "imgur":
-                    response = _bitlyClient.ShortenUrl(_imgurClient.SearchForRandom(formTextContent));
-                    break;
-                case "google books":
-                    response = _googleBooksClient.SearchForAndReturnRandomTextSnippet(formTextContent);
-                    break;
-                case "google vision":
-                    var barchart = _googleVisionClient.AnnotateAndReturnUrlOfBarchart(formTextContent);
-                    response = _bitlyClient.ShortenUrl(barchart);
-                    break;
-                case "send text":
-                    var temp = formTextContent?.Split(new[] {' '}, 2);
-                    var phoneNumber = temp[0].Trim();
-                    var messageText = temp[1].Trim();
-                    response = _textBeltClient.SendMessage(phoneNumber, messageText);
-                    break;
-                case "search repos":
-                    response = $"<{_gitHubClient.GetUrlToCodeSearchOrganizationRepos(formTextContent)}|Search results.>";
-                    break;
-            }
-            return response;
         }
     }
 }
