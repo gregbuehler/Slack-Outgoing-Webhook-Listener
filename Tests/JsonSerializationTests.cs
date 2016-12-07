b664ccc0db98d2a062b2e0684ea7442793e369c2
DumpsterDoofus <dumpsterdoofus@gmail.com>
2016-12-07 13:46:36 -0800

    no message

--- Tests/JsonSerializationTests.cs
+++ Tests/JsonSerializationTests.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using MarioWebService.Enums;
 using MarioWebService.Mappers;
+using MarioWebService.Models;
 using Newtonsoft.Json;
 using NUnit.Framework;
 
@@ -9,18 +10,41 @@ namespace Tests
     [TestFixture]
     class JsonSerializationTests
     {
+        private readonly CustomJsonConverter _converter = new CustomJsonConverter();
 
         [Test]
         [TestCase(ResponseType.InChannel, "\"in_channel\"")]
         [TestCase(ResponseType.Ephemeral, "\"ephemeral\"")]
-        public void ResponseTypeTests(ResponseType responseType, string nameThatSlackExpects)
+        public void ResponseTypeTests(ResponseType responseType, string stringThatSlackExpects)
         {
-            var c = new CustomJsonConverter();
-            var s = JsonConvert.SerializeObject(responseType, c);
-            s.ShouldBeEquivalentTo(nameThatSlackExpects);
+            var s = JsonConvert.SerializeObject(responseType, _converter);
+            s.ShouldBeEquivalentTo(stringThatSlackExpects);
         }
 
-        //[Test]
-        //public void OutgoingWebhookResponseTests()
+        [Test]
+        [TestCase("some text", "{\"text\":\"some text\"}")]
+        public void OutgoingWebhookResponseTests(string text, string stringThatSlackExpects)
+        {
+            var owr = new OutgoingWebhookResponse
+            {
+                Text = text
+            };
+            var s = JsonConvert.SerializeObject(owr, _converter);
+            s.ShouldBeEquivalentTo(stringThatSlackExpects);
+        }
+
+        [Test]
+        [TestCase("some text", ResponseType.InChannel, "{\"response_type\":\"in_channel\",\"text\":\"some text\",\"attachments\":null}")]
+        public void SlashCommandResponseTests(string text, ResponseType responseType, string stringThatSlackExpects)
+        {
+            var owr = new SlashCommandResponse
+            {
+                Text = text,
+                ResponseType = responseType
+            };
+            var s = JsonConvert.SerializeObject(owr, _converter);
+            s.ShouldBeEquivalentTo(stringThatSlackExpects);
+        }
+
     }
 }
